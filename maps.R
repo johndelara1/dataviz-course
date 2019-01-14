87c3f14c2e16d3dbb98f3ebe65ba0a43893ff9b4
Jean Prado <jeangrprado@gmail.com>
2019-01-13 22:32:04 -0200

    updated .gitignore

--- maps.R
+++ maps.R
@@ -10,5 +10,11 @@ library(ggplot2)
 
 # to-do: map with fertility rate per country https://youtu.be/A_0e6SewtTM?list=PL0tDk-f4v1ugEg01UdiXB9QL0AaSSsaYs&t=190
 # world shape file by country: data/world/
+options(device="X11")
+X11.options(type="cairo")
+library(sf)
+
+ggplot(st_read('data/world/TM_WORLD_BORDERS-0.3.shp')) +
+  geom_sf()
 
 # continue course: how to use iNZight https://www.youtube.com/watch?v=ZVh7rPvvUGI&index=20&list=PL0tDk-f4v1ugEg01UdiXB9QL0AaSSsaYs
